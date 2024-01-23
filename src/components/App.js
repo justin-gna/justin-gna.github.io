945ebc0facd2f752d4a3e6e0c7a61acf8a5ba21c
Justin Gnananadchtheram <justinedward8@gmail.com>
2024-01-23 16:25:44 -0500

    restoration

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import NavBar from './NavBar';
+import HomePage from './HomePage';
+import AboutMe from './AboutMe';
+import Projects from './Projects';
+import ExtraStuff from './ExtraStuff';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <HomePage />
+      <AboutMe />
+      <Projects />
+      <ExtraStuff />
+    </div>
+  );
+}
+
+export default App;
